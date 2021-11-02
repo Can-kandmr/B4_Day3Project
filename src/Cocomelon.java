6e4615430b3bbdc8151cbdc192a9fab42b8b059e
Can-kandmr <cansukandemir91@gmail.com>
2021-11-02 17:17:17 +0000

    new message for class cocomelon 2!!!!

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,6 +2,7 @@ public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("lovely day");
         // I love Ice Cream
+        // Cansu is a clever girl I have ever met!!!!
 
     }
 }
