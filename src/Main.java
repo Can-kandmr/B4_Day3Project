4e93c994e3b871bfedce1bd4a06ef717f12a9a13
Can-kandmr <cansukandemir91@gmail.com>
2021-11-02 16:41:06 +0000

    working on nice message 1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello from last Git day");
+        // B4 is excited
+
     }
 }
